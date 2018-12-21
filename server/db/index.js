ee0b702be6fbdb9188b02a8deafb16e0f0c226eb
Jonathan Raitses <jonathan.raitses@gmail.com>
2018-12-20 20:02:14 -0500

    complete backend and starting front end

--- server/db/index.js
+++ server/db/index.js
@@ -10,18 +10,13 @@ var randomObject = (data) => {
   var random = Math.random();
 
   var corgiObj = {
-    image1Url: data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)]
+    image1Url: data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)],
+    image2Url: data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)],
+    image3Url: data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)],
+    image4Url: data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)]
   }
 
   if (random >= 0.5) {
-    var corgiPic2 = data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)];
-    var corgiPic3 = data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)];
-    var corgiPic4 = data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)];
-
-    corgiObj.image2Url = corgiPic2
-    corgiObj.image3Url = corgiPic3
-    corgiObj.image4Url = corgiPic4
-  } else if (random < 0.5) {
     var corgiPic5 = data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)];
     var corgiPic6 = data.picturesOfCorgis[Math.floor(Math.random()*data.picturesOfCorgis.length)];
     var corgiVid = data.videosOfCorgis[Math.floor(Math.random()*data.videosOfCorgis.length)];
@@ -33,7 +28,6 @@ var randomObject = (data) => {
   return corgiObj;
 }
 
-
 connection.connect((err) => {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -41,7 +35,7 @@ connection.connect((err) => {
   }
   console.log('Successfully connected to database.');
   for (let i = 1; i <= 100; i++) {
-    insertCorgi(randomObject(testData));
+    insertCorgi(randomObject(testData), i);
   } //seeder
 });
 
