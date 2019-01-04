0d5c3c4ce47567cf41064baed3ed73758d15a98b
Jonathan Raitses <jonathan.raitses@gmail.com>
2019-01-04 18:28:25 -0500

    deployment fixed

--- server/resources/Models.js
+++ server/resources/Models.js
@@ -1,10 +1,11 @@
 
 var Sequelize = require("sequelize");
 var mysqlConfig = {
-  host     : process.env.HOST,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DATABASE
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_DATABASE
 }
 
 const sequelize = new Sequelize(
