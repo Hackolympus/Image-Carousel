9094ca109112da2133ee25d14a3d264a81c606c8
Jonathan Raitses <jonathan.raitses@gmail.com>
2019-01-05 09:50:42 -0500

    fixed cors issues

--- server/server.js
+++ server/server.js
@@ -4,22 +4,16 @@ var morgan = require('morgan');
 var db = require('./db'); //runs ./db. do not delete.
 var Router = require('./resources/Routers.js');
 var path = require('path');
+var cors = require('cors');
 
 // Create the Express application:
 var app = express();
 
 // Attach middleware:
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../react-client/dist')));
 app.use(morgan('dev'));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-    });
 app.use('/listing', Router);
 
 module.exports = app;
